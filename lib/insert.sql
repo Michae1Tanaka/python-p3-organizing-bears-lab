e77307ebf073f64f16633b5fcca0f8662547d469
Michael Tanaka <tanakam4214@gmail.com>
2023-07-25 22:05:39 -0700

    Lab Completed

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears
+VALUES (1, "Mr. Chocolate", 3, "M", "Black", "Shy", 1),
+    (2, "Rowdy", 7, "M", "Brown", "Aggressive", 1),
+    (3, "Tabitha", 5, "F", "Black", "Friendly", 1),
+    (
+        4,
+        "Sergeant Brown",
+        8,
+        "M",
+        "Brown",
+        "Aggressive",
+        1
+    ),
+    (5, "Melissa", 1, "F", "Black", "Shy", 1),
+    (6, "Grinch", 9, "M", "Green", "Mean", 1),
+    (7, "Wendy", 12, "F", "Black", "Shy", 1),
+    (8, NULL, 6, "M", "Black", "Shy", 1)
